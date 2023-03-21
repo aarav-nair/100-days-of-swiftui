dc73b1954a1840170ddb6d68af1cb4391c4eb6f0
aarav-nair <aarav.nair@gmail.com>
2023-03-21 17:31:30 -0400

    Created Project

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Aarav Nair on 3/21/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
