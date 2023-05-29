be51acb8d06da517e9106b5e5b271343c1183562
aarav-nair <aarav.nair@gmail.com>
2023-05-29 15:24:42 -0400

    Challenge 3

--- Project3/Project3/ContentView.swift
+++ Project3/Project3/ContentView.swift
@@ -7,32 +7,26 @@
 
 import SwiftUI
 
+struct Title: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundColor(.blue)
+    }
+}
+
+extension View {
+    func TitleStyle() -> some View {
+        modifier(Title())
+    }
+}
+
 struct ContentView: View {
     @State private var useRedText = false
-
+    
     var body: some View {
-        VStack {
-            Text("Hello, world!")
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
-                    .background(.red)
-            
-            Text("Hello, world!")
-                .padding()
-                .background(.red)
-                .padding()
-                .background(.blue)
-                .padding()
-                .background(.green)
-                .padding()
-                .background(.yellow)
-        }
-        .padding()
-        
-        Button("Hello World") {
-                    // flip the Boolean between true and false
-                    useRedText.toggle()
-                }
-                .foregroundColor(useRedText ? .red : .blue)
+        Text("Hello World")
+            .TitleStyle()
     }
 }
 
